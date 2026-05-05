f03138910bd6fc42bf58d4e67ed51559fe824b9b
Ryohei-hayashi <62039049+Ryohei-hayashi@users.noreply.github.com>
2026-05-05 14:38:56 +0900

    Delete src directory

--- src/app/diagnosis/page.tsx
+++
@@ -1,7 +0,0 @@
-import { fetchQuestions } from '@/lib/microcms';
-import DiagnosisClient from './DiagnosisClient';
-
-export default async function DiagnosisPage() {
-  const questionsData = await fetchQuestions();
-  return <DiagnosisClient questionsData={questionsData} />;
-}
